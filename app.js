406fd212572328813731d304f8fb3d78500955a1
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-03 12:33:18 -0500

    Made the basic structure of the page , added few items to the store area

---
+++ app.js
@@ -0,0 +1,97 @@
+const shopItem = document.querySelectorAll('.shop-item') ;
+console.log(shopItem)
+const shopBtns= document.querySelectorAll('.btn-dark');
+
+
+shopBtns.forEach(function(shopBtn){
+  shopBtn.addEventListener('click', function(event){
+      // console.log(event.target.classList.contains('btn-dark'))
+      if( event.target.classList.contains('btn-dark')){
+         
+
+        var shopPriceOriginal = event.target.previousElementSibling.previousElementSibling.textContent;
+        var shopPrice = parseFloat(shopPriceOriginal.replace('$', ''))
+        var shopTitle = event.target.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+        var shopImage = event.target.parentElement.parentElement.firstElementChild.src ;
+      
+        const cartItemAdded = {};
+          cartItemAdded.shopPrice = shopPrice;
+          cartItemAdded.shopTitle = shopTitle ;
+          cartItemAdded.shopImage = shopImage;
+          console.log(cartItemAdded)
+          
+
+      const cartItem = document.createElement('div');
+      cartItem.classList.add('row', 'p-4','bg-dark','text-light')
+
+      cartItem.innerHTML = `
+    
+      <div class="col-md-3 ">
+           <img src="${cartItemAdded.shopImage}" alt="cart image" class="img-fluid cart-image w-50">
+           <p class="cart-item-title">${cartItemAdded.shopTitle}</p>
+       </div>
+
+      <div class="col-md-3">
+      <p class="lead cart-item-price">$${cartItemAdded.shopPrice}</p>
+      </div>
+
+      <div class="col-md-3 d-flex justify-content-around">
+        <h3><i class="bi bi-plus-circle"></i></h3>
+        <p class="lead cart-item-quantity">1</p>
+        <h3><i class="bi bi-dash-circle"></i></h3>
+      </div>
+
+      <div class="col-md-3">
+      <h3><i class="bi bi-trash-fill"></i></h3>
+      </div>
+      `
+    //Avoiding the same item from being added to cart twice
+      alert('Item has been added to cart')
+      
+
+    //selecting cart
+
+    const cart = document.getElementById('caart-info');
+    //selecting total amount to be updated
+    const total = document.getElementById('cart-total-amount');
+    const totalCon= document.querySelector('.cart-total');
+
+    cart.append(cartItem)   
+    updateTotal()
+    //Checkout Button Functionanlity
+    
+        };
+    function updateTotal(){
+      total =[];
+        const items = document.querySelectorAll('.cart-item-price');
+
+        items.forEach(function(item){
+          total.push(parseFloat(item.textContent.replace('$',0)));
+        })
+        console.log(total)
+
+        const totalMoney = total.reduce (function(total, item){
+          total += item ;
+          return total
+        })
+        console.log(totalMoney)
+        finalMoney ='$'+ totalMoney.toFixed(2)
+        console.log(finalMoney)
+
+        document.getElementById('cart-total-amount').textContent =finalMoney;
+
+    }
+   
+        
+
+       
+      })
+     
+  })
+  checkoutBtn = document.getElementsByClassName('checkout-btn') ;
+  console.log(checkoutBtn)
+  checkoutBtn.addEventListener('click',showBalance())
+  function showBalance(){
+    console.log("mama")
+  }
+    
